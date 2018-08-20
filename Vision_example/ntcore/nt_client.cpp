3c1603551f6f7c39dc26d81383917f0015cfe6e6
nathanmutin <32647745+nathanmutin@users.noreply.github.com>
2018-08-20 10:40:04 +0200

    Modification nt_client.cpp
    Pour test communication avec roborio

--- Vision_example/ntcore/nt_client.cpp
+++ Vision_example/ntcore/nt_client.cpp
@@ -1,17 +1,34 @@
 #include <ntcore.h>
 #include <iostream>
+#include <string>
+
 #include <networktables/NetworkTable.h>
 #include <networktables/NetworkTableInstance.h>
 
-int main() {
-  auto inst = nt::NetworkTableInstance::GetDefault();
-  inst.StartClient("localhost", 1735);
+int main()
+{
+	  auto inst = nt::NetworkTableInstance::GetDefault();
+	  auto table = inst.GetTable("datatable");
+	  auto entry = table->GetEntry("Value");
+  
+  inst.StartClientTeam(5553);
 
-  while (true) {
-    auto entry = inst.GetEntry("/Value");
-    if (entry.Exists()) {
-      std::cout << entry.GetString("not found") << std::endl;
-      break;
+  while (true)
+  {
+    if (entry.Exists())
+	{
+	  std::string value = entry.GetString("not found");
+	  
+	  if(value == "Hello RaspberryPi !!!")
+	  {
+		  std::cout << "Le message \"Hello RaspberryPi !!!\" a été reçu" << std::endl << std::endl;
+		  std::cout << "Envoi du message \"Hello RoboRIO !!!\" ..." << std::endl << std::endl;
+		  entry.SetString("Hello RoboRIO !!!");
+	  }
+	  else
+	  {
+		  std::cout << value << std::endl << std::endl;
+	  }
     }
   }
   return 0;
