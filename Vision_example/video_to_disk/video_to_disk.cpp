af180bd35ebd641e3cb291ac5aef6dcb70c218d2
Nathan <nathan.mutin@gmail.com>
2018-11-11 16:26:19 +0000

    Variables hauteur largeur fps

--- Vision_example/video_to_disk/video_to_disk.cpp
+++ Vision_example/video_to_disk/video_to_disk.cpp
@@ -7,30 +7,40 @@ using namespace cs;
 using namespace cv;
 
 int main() {
+	
+	int width = 320;
+	int height = 240;
+	int fps = 15;
+	
 	UsbCamera camera("usbcam", 0);
 	
 	//SetVideoMode (VideoMode::PixelFormat pixelFormat, int width, int height, int fps)
-	camera.SetVideoMode(VideoMode::PixelFormat::kMJPEG, 320, 240, 15);
+	camera.SetVideoMode(VideoMode::PixelFormat::kMJPEG, width, height, fps);
 	
 	MjpegServer mjpegServer("httpserver", 8081);
 	mjpegServer.SetSource(camera);
 	CvSink cvsink("cvsink");
 	cvsink.SetSource(camera);
 	CvSource cvsource("cvsource",
-			VideoMode::PixelFormat::kMJPEG, 320, 240, 15);
+			VideoMode::PixelFormat::kMJPEG, width, height, fps);
 	MjpegServer cvMjpegServer("cvhttpserver", 8082);
 	cvMjpegServer.SetSource(cvsource);
 
-	Size frameSize(320, 240);
-	VideoWriter videoWriter("output.avi",
-			VideoWriter::fourcc('F', 'M', 'P', '4'), 15.0, frameSize, true);
+	VideoWriter videoWriter("output.avi", VideoWriter::fourcc('F', 'M', 'P', '4'), fps, Size(width, height), true);	
+	
+	if (!videoWriter.isOpened())
+	{
+		std::cout << "Cannot open output file " << std::endl;
+
+		return 1;
+	}
 
 	Mat frame;
 
 	int count = 0;
 	while (count < 100) {
 		uint64_t time = cvsink.GrabFrame(frame);
-		printf("Frame Time:  %lld\n", time);
+		std::cout << "Frame Time: " << time << std::endl;
 		videoWriter.write(frame);
 		count++;
 	}
