7f9ad5449af44dbc1637a641508f0630c0141042
Nathan <nathan.mutin@gmail.com>
2018-12-27 14:18:37 +0000

    Changement packages et options cmake

--- Raspberry_init.bash
+++ Raspberry_init.bash
@@ -1,29 +1,22 @@
 #!/bin/bash
 
-echo  
 echo =======================================================================================
 echo ======================== Ensure that Raspbian is all up-to-date =======================
 echo =======================================================================================
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
-cd ~
+
 
 echo  
 echo =======================================================================================
-echo ========================= Miscellaneous distribution installs =========================
+echo ============================= Required Packages for Opencv ============================
 echo =======================================================================================
 
-sudo apt-get -y install g++
-sudo apt-get -y install git
-sudo apt-get -y install git-gui
-sudo apt-get -y install geany
-sudo apt-get -y install ftp
-sudo apt-get -y install libboost1.62-all
-sudo apt-get -y install libgtk2.0-dev
-sudo apt-get -y install pkg-config
-sudo apt-get -y install ntp
-sudo apt-get -y install cmake
+sudo apt-get -y install build-essential
+sudo apt-get -y install git cmake pkg-config
+sudo apt-get -y install libgtk2.0-dev libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get -y install libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev
 
 
 echo  
@@ -35,20 +28,30 @@ cd ~
 
 OPENCV_VER="3.4.3"
 sudo wget https://github.com/opencv/opencv/archive/${OPENCV_VER}.zip
-mv ${OPENCV_VER}.zip opencv.zip
+unzip ${OPENCV_VER}.zip
 
-unzip opencv.zip
 cd opencv-${OPENCV_VER}/
-
 mkdir build
 cd build
 
-cmake -DENABLE_NEON=ON -DENABLE_VFPV3=ON -D CMAKE_BUILD_TYPE=RELEASE -D BUILD_PERF_TESTS=OFF -D BUILD_SHARED_LIBRARY=ON -D BUILD_TESTS=OFF -D BUILD_opencv_java=OFF -D BUILD_opencv_python2=OFF -D BUILD_opencv_python3=OFF ~/opencv-${OPENCV_VER}
+cmake -DENABLE_NEON=ON \
+	-DENABLE_VFPV3=ON \
+	-DCMAKE_BUILD_TYPE=RELEASE \
+	-DBUILD_SHARED_LIBRARY=ON \
+	-DBUILD_opencv_java=OFF \
+	-DBUILD_JAVA=OFF \
+	-DBUILD_opencv_python2=OFF \
+	-DBUILD_opencv_python3=OFF \
+	-DBUILD_EXAMPLES=OFF \
+	-DBUILD_TESTS=OFF \
+	-DBUILD_PERF_TESTS=OFF \
+	..
+	  
 make -j5
 sudo make install
 
-cd ../..
-rm -f opencv.zip
+cd ~
+rm -f ${OPENCV_VER}.zip
 
 
 echo  
